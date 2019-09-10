8c80ceca5a6daf4f10526f4480f3f89115debe77
Nziranziza <nziranzizadaniel@gmail.com>
2019-09-10 11:04:40 +0200

    Fix invalid token error
    - add try and catch function where necessary
    
    [finishes #168386594]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -50,24 +50,31 @@ export const getUser = function() {
   this.response.setHeader('Content-type', 'application/json');
   const { id } = this.params;
   const { authorization } = this.request.headers;
-  const { _id, userType } = Jwt.verify(authorization, SECURITY_KEY);
-  if (userType !== 'admin' && id !== _id) {
+  try {
+    const { _id, userType } = Jwt.verify(authorization, SECURITY_KEY);
+    if (userType !== 'admin' && id !== _id) {
+      this.response.writeHead(401);
+      this.response.end(JSON.stringify({
+        message: 'not authorized',
+      }));
+      return;
+    }
+    const user = userCollection.findOne({ _id: id });
+    if (!user) {
+      this.response.writeHead(404);
+      this.response.end(JSON.stringify({
+        message: 'user not found',
+      }));
+      return;
+    }
+    this.response.writeHead(200);
+    this.response.end(JSON.stringify(user));
+  } catch (error) {
     this.response.writeHead(401);
     this.response.end(JSON.stringify({
       message: 'not authorized',
     }));
-    return;
   }
-  const user = userCollection.findOne({ _id: id });
-  if (!user) {
-    this.response.writeHead(404);
-    this.response.end(JSON.stringify({
-      message: 'user not found',
-    }));
-    return;
-  }
-  this.response.writeHead(200);
-  this.response.end(JSON.stringify(user));
 };
 
 /**
@@ -145,52 +152,59 @@ export const postUser = async function() {
 export const login = async function () {
   const { body } = this.request;
   this.response.setHeader('Content-Type', 'application/json');
-  const { error } = Joi.validate({ ...body }, loginBody);
-  if (error) {
-    this.response.writeHead(400);
-    this.response.end(JSON.stringify(error));
-    return;
-  }
-  const { username, password } = body;
-  const user = userCollection.findOne(
-    {
-      $or: [{ email: username },
-        { phoneNumber: username },
-        { username }],
-    },
-  );
-  if (!user) {
-    this.response.writeHead(404);
+  try {
+    const { error } = Joi.validate({ ...body }, loginBody);
+    if (error) {
+      this.response.writeHead(400);
+      this.response.end(JSON.stringify(error));
+      return;
+    }
+    const { username, password } = body;
+    const user = userCollection.findOne(
+      {
+        $or: [{ email: username },
+          { phoneNumber: username },
+          { username }],
+      },
+    );
+    if (!user) {
+      this.response.writeHead(404);
+      this.response.end(JSON.stringify({
+        message: 'password and email don\'t match',
+      }));
+      return;
+    }
+    if (user && user.firstLogin && user.password === password) {
+      this.response.writeHead(200);
+      this.response.end(JSON.stringify({
+        message: 'Reset your password',
+      }));
+      return;
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      this.response.writeHead(404);
+      this.response.end(JSON.stringify({
+        message: 'password and email don\'t match',
+      }));
+    } else {
+      const token = jwtTokenSigner(user);
+      // eslint-disable-next-line no-shadow
+      const { password, ...userData } = user;
+      tokenCollection.insert({
+        userId: user._id,
+        token,
+        createdAt: new Date(),
+        status: 'valid',
+      });
+      this.response.writeHead(200);
+      this.response.end(JSON.stringify({ user: userData, token }));
+    }
+  } catch (err) {
     this.response.end(JSON.stringify({
-      message: 'password and email don\'t match',
+      message: 'something went wrong',
+      error: err,
     }));
-    return;
-  }
-  if (user && user.firstLogin && user.password === password) {
-    this.response.writeHead(200);
-    this.response.end(JSON.stringify({
-      message: 'Reset your password',
-    }));
-    return;
-  }
-  const isValidPassword = await bcrypt.compare(password, user.password);
-  if (!isValidPassword) {
-    this.response.writeHead(404);
-    this.response.end(JSON.stringify({
-      message: 'password and email don\'t match',
-    }));
-  } else {
-    const token = jwtTokenSigner(user);
-    // eslint-disable-next-line no-shadow
-    const { password, ...userData } = user;
-    tokenCollection.insert({
-      userId: user._id,
-      token,
-      createdAt: new Date(),
-      status: 'valid',
-    });
-    this.response.writeHead(200);
-    this.response.end(JSON.stringify({ user: userData, token }));
   }
 };
 /**
